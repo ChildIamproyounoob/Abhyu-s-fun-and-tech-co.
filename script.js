eae4e4802c1c9e212e81bcf42f26c57091e80ad4
Abhyudaya Chaudhary <90612538+ChildIamproyounoob@users.noreply.github.com>
2022-04-22 14:05:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hey!, thanks for inspecting the page, you just found a secret!, here's a cool trick");
+
+a = 20;
+
+for (let i = 0; i < 11; i++){
+    console.log(a*i);
+}
+
+console.log("the table of 20, all done with expert programming! :)")
